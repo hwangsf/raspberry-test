0e4540dabce8062b83e3b856084efd00654814ab
Dean Huang <hwangsf@gmail.com>
2023-10-25 15:15:11 +0800

    modify

---
+++ lesson7.py
@@ -0,0 +1,12 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(24)
+# led = LED("GPIO27")   # 給 GPIO 腳位，跟上一行意義相同！
+# led = LED("J8:13")    # 定義為樹莓派的腳位
+
+while True:
+  led.on()
+  sleep(1)
+  led.off()
+  sleep(1)
